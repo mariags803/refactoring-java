db4fff1cd23bdcdd14381bc64b8f056006c79016
Miguel Ignacio Cabrera San Gil <miguelcabsan@gmail.com>
2024-05-06 17:05:36 +0100

    :sparkles: initial commit

---
+++ src/test/java/MainShould.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MainShould {
+    @Test
+    void fail() {
+        assertThat(1).isEqualTo(2);
+    }
+}
